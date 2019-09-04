1bd45c5999a3fca6c6c504c1656ef7733a167b9e
Lucas VIeira <lucas.lsv.ti@gmail.com>
2019-09-04 12:34:44 -0300

    estrutura base

---
+++ src/main/java/com/br/listavip/configurations/ListavipApplication.java
@@ -0,0 +1,13 @@
+package com.br.listavip.configurations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListavipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListavipApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/br/listavip/controllers/convidadoController.java
@@ -0,0 +1,5 @@
+package com.br.listavip.controllers;
+
+public class convidadoController {
+
+}
